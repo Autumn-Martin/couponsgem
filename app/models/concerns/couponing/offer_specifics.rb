9bb1a17f14e4f45158568e26719848deb5bb688f
Autumn Martin <autumn.the.dev@gmail.com>
2022-07-23 21:14:14 -0600

    Address more PR feedback

--- app/models/concerns/couponing/offer_specifics.rb
+++ app/models/concerns/couponing/offer_specifics.rb
@@ -1,15 +1,9 @@
 module Couponing::OfferSpecifics
   extend ActiveSupport::Concern
 
-  included do
-    has_many :offers, dependent: :destroy
-
-    accepts_nested_attributes_for :offers, allow_destroy: true
-  
-    validates :offers, presence: true
+  def offer_list
+    ""
   end
-
-  def offer_list; end
   def available_offers
     []
   end
